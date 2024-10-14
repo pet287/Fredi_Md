31310e74f669af9f19dfd9cba6a656ec56a4b296
pet287 <petermikefb911@gmail.com>
2024-10-14 12:49:49 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,8 +19,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "©Fredie",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "255745086279",
+  OWNER_NAME: process.env.OWNER_NAME || "❌Ǥ҉Ø҉Đ҉W҉Ɨ҉N҉❌",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "2349012464012",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : true, 
 };
